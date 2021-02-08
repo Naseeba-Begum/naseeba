58a8ad6eaff7243bda485bfa801d247648cdb0da
Naseeba-Begum <75074138+Naseeba-Begum@users.noreply.github.com>
2021-02-08 09:24:04 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
- 
-int add(int , int );
+
 int main()
 {
     int num1, num2, res;
@@ -9,14 +7,8 @@ int main()
     scanf("%d", &num1);
     printf("enter Second Number:\t");
     scanf("%d", &num2);
-    res=add(num1,num2);
+    res=num1+num2;
     printf("\nAddition of Two Numbers: %d", res);
     
     return 0;
-}
- 
-int add(int x, int y)
-{
-    int res = x + y;
-    return res;
 }
