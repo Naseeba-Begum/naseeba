e3af0cfeb1e833b250daa6967bdb6124bebbe83e
Naseeba-Begum <75074138+Naseeba-Begum@users.noreply.github.com>
2021-02-08 09:59:31 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -8,7 +8,7 @@ int main()
     printf("enter Second Number:\t");
     scanf("%d", &num2);
     res=num1+num2;
-    printf("\nAddition of Two Numbers: %d", res);
+    printf("\n Addition of %d and %d is %d\n ",num1,num2, res);
     
     return 0;
 }
