d330898f8f9790c72c6fb9102469da940ab28967
Naseeba-Begum <75074138+Naseeba-Begum@users.noreply.github.com>
2021-02-20 17:09:41 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,39 +2,39 @@
 #include<stdio.h>
 struct frac
 {
-int numerator,denomerator;
+    int numerator,denomerator;
 };
 typedef struct frac fraction;
 fraction input()
 {
-fraction n;
-printf("enter numerator : ");
-scanf("%d",&n.numerator);
-printf("enter denomerator");
-scanf("%d",&n.denomerator);
-return n;
+    fraction n;
+    printf("enter numerator : ");
+    scanf("%d",&n.numerator);
+    printf("enter denomerator");
+    scanf("%d",&n.denomerator);
+    return n;
 }
 fraction add(fraction a, fraction b)
 {
-fraction result;
+    fraction result;
 if(a.denomerator==b.denomerator)
 {
-result.denomerator=a.denomerator;
-result.numerator=a.numerator+b.numerator;
+    result.denomerator=a.denomerator;
+    result.numerator=a.numerator+b.numerator;
 }
 else
 {
-result.denomerator=a.denomerator*b.denomerator;
-result.numerator=(a.numerator*b.denomerator)+(b.numerator*a.denomerator);
+    result.denomerator=a.denomerator*b.denomerator;
+    result.numerator=(a.numerator*b.denomerator)+(b.numerator*a.denomerator);
 }
-return result;
+    return result;
 }
 int main()
 {
-fraction a,b,c;
-a=input();
-b=input();
-c=add(a,b);
-printf("the sum of two fraction given numerator and denomerator  %d, %d and %d,%d is %d /%d ",a.numerator,a.denomerator,b.numerator,b.denomerator,c.numerator, c.denomerator);
-return 0;
+    fraction a,b,c;
+    a=input();
+    b=input();
+    c=add(a,b);
+    printf("the sum of two fraction given numerator and denomerator %d,%d and %d,%d is %d /%d ",a.numerator,a.denomerator,b.numerator,b.denomerator,c.numerator, c.denomerator);
+    return 0;
 }
