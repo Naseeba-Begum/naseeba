7fdc33d945446062debc799c9141458b8a2274b5
Naseeba-Begum <75074138+Naseeba-Begum@users.noreply.github.com>
2021-04-10 11:31:41 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -4,48 +4,70 @@ struct frac
 {
     int n,d;
 };
+
 typedef struct frac fraction;
-fraction input()
+
+int input_size()
 {
-    fraction a;
-    printf("enter numerator :");
-    scanf("%d",&a.n);
-    printf("enter denomerator");
-    scanf("%d",&a.d);
+    int a;
+    printf("enter the maximum num of fraction here : \n");
+    scanf("%d",&a);
     return a;
 }
-fraction add(fraction a , fraction b)
+void input(int n , fraction f[n])
 {
-    fraction result;
-    if(a.d==b.d)
-    {
-         result.d=a.d;
-         result.n=a.n+b.n;
+     printf("enter the numerator  and the denominator here : \n");
+     for (int i=0; i<n; i++)
+     {
+         scanf("%d%d",&f[i].n,&f[i].d);
      }
-    else
-    {
-        result.d=a.d*b.d;
-        result.n=(a.n+b.d)+(b.n+a.d);
-    }
-return result;
+}
+int gcd(int n, int m)
+{
+    if(m==0)
+        return n;
+    return gcd(m , n%m);
+}
+
+fraction compute_two_fractions(fraction a , fraction b)
+{
+    fraction c;
+    c.d = a.d * b.d;
+    c.n = (a.n)*(b.d)+(b.n)*(a.d);
+    int  e = gcd(c.n ,c.d);// e is  common factor//
+    c.d = c.d/e;
+    c.n = c.n/e;
+    return c;
  }
+
+fraction compute_n_fractions(int  n , fraction f[n])
+{
+    fraction sum;
+    sum.n = 0;
+    sum.d = 1;
+    for(int i =0; i<n ; i++)
+    {
+        sum = compute_two_fractions(sum , f[i]);
+    }
+   return sum;
+}
+
+void result (int n , fraction f[n] ,fraction sum) 
+{
+    printf("the fractions that to be  added up are: \n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d / %d \n" , f[i].n ,f[i].d);
+    }
+    printf("the sum is %d / %d", sum.n , sum.d);
+}
 int main()
 {
-    int i;
-    printf("enter the number of fractions that to be added :");
-    scanf("%d",&i);
-    fraction s,a[i];
-    s.n=0;
-    s.d=1;
-    for(int k=0; k<i; k++)
-    {
-        printf("for the fraction %d \n",(k+1));
-        a[k]=input();
-    }
-    for(int k=0;k<i;k++)
-    {
-        s=add(s,a[k]);
-    }
-    printf("the addition of the given fractions is  %d / %d ",s.n,s.d);
+    int a;
+    a = input_size();
+    fraction f[a] , sum;
+    input(a , f);
+    sum = compute_n_fractions(a,f);
+    result(a , f , sum);
     return 0;
 }
